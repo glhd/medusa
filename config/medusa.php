ceebdf95026065dbc339fb038a71a100a7107d54
Chris Morrell <chris@cmorrell.com>
2019-02-25 21:26:47 -0500

    Routing WIP

--- config/medusa.php
+++ config/medusa.php
@@ -3,6 +3,41 @@
 return [
 	/*
 	|--------------------------------------------------------------------------
+	| Medusa Path
+	|--------------------------------------------------------------------------
+	|
+	| This is the root path to serve Medusa from.
+	|
+	*/
+	
+	'path' => env('MEDUSA_PATH', '/medusa'),
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Default Guard
+	|--------------------------------------------------------------------------
+	|
+	| This is the guard that Medusa should use when resolving users and
+	| checking permissions (defaults to the Laravel "web" guard).
+	|
+	*/
+	
+	'guard' => env('MEDUSA_GUARD', 'web'),
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Default Middleware
+	|--------------------------------------------------------------------------
+	|
+	| Middleware to pass Medusa requests through. Should at least use auth
+	| middleware of some sort.
+	|
+	*/
+	
+	'middleware' => ['web'],
+	
+	/*
+	|--------------------------------------------------------------------------
 	| Content Model
 	|--------------------------------------------------------------------------
 	|
--- src/Support/MedusaServiceProvider.php
+++ src/Support/MedusaServiceProvider.php
@@ -4,21 +4,31 @@ namespace Galahad\Medusa\Support;
 
 use Galahad\Medusa\Contracts\Content;
 use Galahad\Medusa\Exceptions\ConfigurationException;
+use Galahad\Medusa\Http\Controllers\ContentController;
+use Galahad\Medusa\Http\Middleware\Authorize;
+use Galahad\Medusa\Http\Middleware\DispatchMedusaEvent;
+use Galahad\Medusa\Http\Middleware\ServeInterface;
 use Galahad\Medusa\Medusa;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\Registrar;
+use Illuminate\Routing\Router;
+use Illuminate\Routing\RouteRegistrar;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 
 class MedusaServiceProvider extends ServiceProvider
 {
 	public function boot() : void
 	{
-		require_once __DIR__.'helpers.php';
+		require_once rtrim(__DIR__, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'helpers.php';
+		
+		$this->bootConfig();
 		
 		$this->bootPermissions($this->app->make(Gate::class));
-		$this->bootConfig();
+		$this->bootRoutes($this->app->make(Registrar::class));
 	}
 	
 	/**
@@ -38,18 +48,29 @@ class MedusaServiceProvider extends ServiceProvider
 		$this->app->alias('galahad.medusa', Medusa::class);
 		
 		$content_model = $this->app->make(Repository::class)->get('medusa.content_model');
-		$content_contract = Content::class;
-		if (!is_subclass_of($content_model, $content_contract)) {
-			throw new ConfigurationException("'{$content_model}' must implement '$content_contract'");
+		if (class_exists($content_model)) {
+			$content_contract = Content::class;
+			if (!is_a($content_model, $content_contract)) {
+				throw new ConfigurationException("'{$content_model}' must implement '$content_contract'");
+			}
+			
+			$this->app->alias($content_model, Content::class);
 		}
-		
-		$this->app->alias($content_model, Content::class);
 	}
 	
+	/**
+	 * Like Laravel Nova, Medusa allows access in local development by default
+	 *
+	 * @param \Illuminate\Contracts\Auth\Access\Gate $gate
+	 */
 	protected function bootPermissions(Gate $gate) : void
 	{
 		$gate->define('_viewMedusa', function() use ($gate) {
-			return $gate->authorize('viewMedusa');
+			if ($gate->has('viewMedusa')) {
+				return $gate->authorize('viewMedusa');
+			}
+			
+			return $this->app->isLocal();
 		});
 	}
 	
@@ -64,6 +85,21 @@ class MedusaServiceProvider extends ServiceProvider
 		return $this;
 	}
 	
+	protected function bootRoutes(Registrar $registrar) : self
+	{
+		$group = [
+			'middleware' => $this->config('middleware'),
+			'prefix' => $this->config('path'),
+			'as' => 'medusa.',
+		];
+		
+		$registrar->group($group, function() use ($registrar) {
+			$registrar->resource('content', ContentController::class);
+		});
+		
+		return $this;
+	}
+	
 	protected function basePath(string $path = null) : string
 	{
 		$base_path = rtrim(dirname(__DIR__, 2), DIRECTORY_SEPARATOR);
@@ -71,5 +107,10 @@ class MedusaServiceProvider extends ServiceProvider
 		return null === $path
 			? $base_path
 			: $base_path.DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR);
+	}
+	
+	protected function config($key, $default = null)
+	{
+		return $this->app->make(Repository::class)->get("medusa.{$key}", $default);
 	}
 }
