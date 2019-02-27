a8bd0d4713a7cbb6aea2fe634b89d8337967da6c
Chris Morrell <chris@cmorrell.com>
2019-02-26 22:55:06 -0500

    A tiny bit more

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const load = require.context('./Fields', false, /^(?!.*\.stories\.js$).*\.js$/i);
 
 export default ({ config }) => {
+	const [data, setData] = useState(() => {
+		const data = {};
+		
+		Object.values(config.fields).forEach(field => {
+			data[field.name] = field.initial_value;
+		});
+		
+		return data;
+	});
+	
 	const fields = Object.values(config.fields)
 		.map(field => {
 			const Field = load('./' + field.component + '.js').default;
-			return <Field { ...field } />;
+			const onChange = (value) => setData({
+				...data,
+				[field.name]: value,
+			});
+			return <Field key={field.name} { ...field } value={data[field.name]} onChange={onChange} />;
 		});
 	
 	return (
 		<div>
 			{ fields }
+			<pre className="bg-grey-lightest border shadow-lg text-xs text-grey-darker font-mono rounded p-8 my-8">{ JSON.stringify(data, null, 2) }</pre>
 			<pre className="bg-grey-lightest border shadow-lg text-xs text-grey-darker font-mono rounded p-8 my-8">{ JSON.stringify(config.rules, null, 2) }</pre>
 		</div>
 	);
--- resources/js/components/Fields/Text.js
+++ resources/js/components/Fields/Text.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-export default (config) => {
+export default (props) => {
 	return (
 		<div>
-			<pre className="bg-grey-lightest border shadow-lg text-xs text-grey-darker font-mono rounded p-8 my-8">{ JSON.stringify(config, null, 2) }</pre>
+			<pre className="bg-grey-lightest border shadow-lg text-xs text-grey-darker font-mono rounded p-8 my-8">
+				{ JSON.stringify(props, null, 2) }
+			</pre>
+			<input
+				className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+				value={props.value}
+				onChange={e => props.onChange(e.target.value)}
+			/>
 		</div>
 	);
 };
